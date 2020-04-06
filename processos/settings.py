4012e793562a737aedcd6003c4a94cd48066ffe9
Camila <imcamilafranco@gmail.com>
2020-04-06 14:42:50 -0300

    pandas version

--- processos/settings.py
+++ processos/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = with open('./secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+
+SECRET_KEY = 'fra5tqukbkny6at5su_aa30^xs%ogcabrul-bh0n+ybs0*ck-dowunco'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
