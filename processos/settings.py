79c38948eec0e6cce4e3948d644d4d30c539d6c9
Camila <imcamilafranco@gmail.com>
2020-04-07 19:06:08 -0300

    fixing settings.py

--- processos/settings.py
+++ processos/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'processos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT')
     }
 }
 
