c837f6777ade852a2dd5b893da74ce6537c9e667
Camila <imcamilafranco@gmail.com>
2020-04-16 19:51:07 -0300

    testing send e-mail with celery.

--- consulta_a_processos/tasks.py
+++ consulta_a_processos/tasks.py
@@ -15,18 +15,26 @@ def hello():
             for ele in re.findall(r'\b\d+\b', str(incidente)):
             #transforma-se esse a lista resultante em string:
                 incidente_string += ele
+                current_model = Processos.objects.get(incidente_id=incidente_string)
+    
                 #atualiza-se a data e o andamento dos processos:                
-                data_atualizacao = get_data_atualizacao(incidente_string)
-                print(data_atualizacao)
-                descricao_atualizacao = get_descricao_atualizacao(incidente_string)
-                print(descricao_atualizacao)
+                nova_data_atualizacao = get_data_atualizacao(incidente_string)
+                print(nova_data_atualizacao)
+                nova_descricao_atualizacao = get_descricao_atualizacao(incidente_string)
+                print(nova_descricao_atualizacao)
+                if (nova_data_atualizacao != current_model.data_atualizacao) or (nova_descricao_atualizacao != current_model.descricao_atualizacao):
+                    print("hehe")
                 #então, pega-se cada incidente
-                update = Processos.objects.get(incidente_id=incidente_string)
+                    #update = Processos.objects.get(incidente_id=incidente_string)
                 #atualiza-se os fields na database
-                update.data_atualizacao = data_atualizacao
-                update.descricao_atualizacao = descricao_atualizacao
+                    current_model.data_atualizacao = nova_data_atualizacao
+                    current_model.descricao_atualizacao = nova_descricao_atualizacao
                 #fields são salvos na database
-                update.save()
+                    try:
+                        enviar_emails(str(current_model.classe), str(current_model.numero), str(current_model.descricao), str(current_model.descricao_atualizacao), str(current_model.data_atualizacao), incidente_string, str(current_model.emails))
+                    except Exception as e:
+                        print(e)
+                    current_model.save()
                 incidente_string = ""
     except Exception as e:
         print("Erro no update!")
--- consulta_a_processos/utils.py
+++ consulta_a_processos/utils.py
@@ -8,43 +8,13 @@ import requests
 from bs4 import BeautifulSoup
 import csv   
 from csv import writer
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #Aqui tem todos os métodos do site.
-
-def save_table(classe, numero, descricao, emails):
-    incidente_id = get_incidente_id(classe, numero)
-    client = requests.session()
-    url_find = "http://portal.stf.jus.br/processos/detalhe.asp?incidente=" + incidente_id
-    url = "http://portal.stf.jus.br/processos/abaAndamentos.asp?incidente=" + incidente_id 
-    response_andamento = client.get(url)
-    print("- - - Get data atualização")
-    html_page = response_andamento.content
-
-    soup = BeautifulSoup(html_page, 'html.parser')
-
-    andamento_data = soup.find("div", {"class": "andamento-data"}).get_text()
-    andamento_nome = soup.find("h5", {"class": "andamento-nome"}).get_text()
-
-    #with open('resultados.csv', 'a+', newline='') as write_obj:
-    #            # Create a writer object from csv module
-    #            csv_writer = writer(write_obj)
-                # Add contents of list as last row in the csv file
-    #            csv_writer.writerow(classe, numero, incidente_id, descricao, andamento_data, andamento_nome, emails, url_find)
-    #with open('resultados_novos.csv', 'a+', newline='') as write_obj:
-                # Create a writer object from csv module
-    #            csv_writer = writer(write_obj)
-                # Add contents of list as last row in the csv file
-    #            csv_writer.writerow(classe, numero, incidente_id, descricao, andamento_data, andamento_nome, emails, url_find)
-    fields=[classe, numero, incidente_id, descricao, andamento_data, andamento_nome, emails, url_find]
-    with open(r'resultados.csv', 'a', encoding = 'utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
-    with open(r'resultados_novos.csv', 'a', encoding = 'utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
-    return
 
 def get_incidente_id(classe, numero):
     client = requests.session()
@@ -82,46 +52,47 @@ def get_descricao_atualizacao(incidente_id):
     andamento_nome = soup.find("h5", {"class": "andamento-nome"}).get_text()    
     return andamento_nome 
 
-def atualizar_tabela_cadastro():
-    arquivo_processos = "C:\\Users\\pedro\\Desktop\\processos_app\\lista_de_processos.csv"
-    processos_df = pd.read_csv(arquivo_processos)
+def enviar_emails(classe, numero, descricao, descricao_atualizacao, data_atualizacao, incidente_id, email):
+    # Conexao com o servidor do Gmail, utilizando login e senha:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('processosglobobsa@gmail.com', 'TVG@bsa#processos')
 
-    datasets = []
-    headings = ['Classe', 'Número', 'ID', 'Descrição', 'Data da Última Atualização', 'Descrição da Última Atualização', 'E-mail']
-    datasets.append(headings)
+        # Informacoes de remetentes e destinatarios:
+    sender = 'processosglobobsa@gmail.com'
+        #to_addresses = 'mvjunior@g.globo, fernanda.vivas@g.globo, rosanne.dagostino@g.globo'
+        #cc_addresses = 'camila.franco@g.globo, cassio.fabius@g.globo'
+        #bcc_addresses = 'adonias.melo@g.globo, esousa@g.globo, lgcarvalho@g.globo, matheus.moreira@g.globo'
+        #recipients_addresses = to_addresses.split(",") + cc_addresses.split(",") + bcc_addresses.split(",")
+    to_addresses = email
+    recipients_addresses = to_addresses
+        # Assunto do e-mail:
+    subject = 'Atualização de processos do STF: ' + str(descricao)
 
-    client = requests.session()
-
-    try:
-        for index, row in processos_df.iterrows():
-            classe = row.classe
-            numero = str(row.numero)
+        # Corpo do e-mail:
+    message = '<html><body><p>Olá!</p><p>O seguinte processo foi atualizado: ' + str(classe) + ' ' + str(numero) + \
+                '</p><p>Descrição da atualização: ' + str(descricao_atualizacao) + ' em: ' + str(data_atualizacao) + \
+                '.</p><p>Para acompanhar o processo, <a href="http://portal.stf.jus.br/processos/detalhe.asp?incidente=' + str(incidente_id) + '">clique aqui</a>.</p>' + \
+                '<p>Atenciosamente, Tecnologia Brasília.</p></body></html>' 
         
-            incidente_id = get_incidente_id(classe, numero)
-            print("- - - Get incidente number")
-            url = "http://portal.stf.jus.br/processos/abaAndamentos.asp?incidente=" + incidente_id
-            response_andamento = client.get(url)
-            print("- - - Get incidente")
-            html_page = response_andamento.content
-    
-            soup = BeautifulSoup(html_page, 'html.parser')
-    
-            andamento_data = soup.find("div", {"class": "andamento-data"}).get_text()
-            andamento_nome = soup.find("h5", {"class": "andamento-nome"}).get_text()
-    
-            dataset = [classe, numero, incidente_id, row.descricao, andamento_data, andamento_nome, row.emails]
-            datasets.append(dataset)
+        # Montagem do e-mail:
+    msg = MIMEMultipart()
 
-    except Exception as e:
-        print("Erro no scrapping!")
-        print(e)
+    msg['From'] = sender
+    msg['To'] = to_addresses
+    #msg['Cc'] = cc_addresses
+    msg['Subject'] = subject
 
-    headers = datasets.pop(0)
-    export_df = DataFrame(datasets, columns=headers)
-    filename = "C:\\Users\\pedro\\Desktop\\processos_app\\resultados.csv"
-    export_df.to_csv(filename,
-			    index = False,
-			    sep = ',',
-			    encoding = 'utf-8'
-		    )
+    msg.attach(MIMEText(message, 'html', 'utf-8'))
 
+    text = msg.as_string()
+
+        # Envio do e-mail para os destinatarios:
+    server.sendmail(sender, recipients_addresses, text)
+
+    # Finalizacao da conexao com o Gmail apos o envio de todos os e-mails:
+    server.quit()
+
+    # Limpa o arquivo "updates.csv" para a proxima consulta:
+
+    return
