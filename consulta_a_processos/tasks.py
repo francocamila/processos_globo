d9c0ec37dd233a4eab7e7e0253e7e2c7113f8ac8
Camila <imcamilafranco@gmail.com>
2020-04-17 18:40:42 -0300

    now I guess email_sender from db will work

--- consulta_a_processos/tasks.py
+++ consulta_a_processos/tasks.py
@@ -22,7 +22,7 @@ def hello():
                 print(nova_data_atualizacao)
                 nova_descricao_atualizacao = get_descricao_atualizacao(incidente_string)
                 print(nova_descricao_atualizacao)
-                if (nova_data_atualizacao != current_model.data_atualizacao) or (nova_descricao_atualizacao != current_model.descricao_atualizacao):
+                if (nova_data_atualizacao != str(current_model.data_atualizacao)) or (nova_descricao_atualizacao != str(current_model.descricao_atualizacao)):
                     print("hehe")
                 #então, pega-se cada incidente
                     #update = Processos.objects.get(incidente_id=incidente_string)
@@ -30,10 +30,10 @@ def hello():
                     current_model.data_atualizacao = nova_data_atualizacao
                     current_model.descricao_atualizacao = nova_descricao_atualizacao
                 #fields são salvos na database
-                    try:
-                        enviar_emails(str(current_model.classe), str(current_model.numero), str(current_model.descricao), str(current_model.descricao_atualizacao), str(current_model.data_atualizacao), incidente_string, str(current_model.emails))
-                    except Exception as e:
-                        print(e)
+                    #try:
+                    enviar_emails(str(current_model.classe), str(current_model.numero), str(current_model.descricao), str(current_model.descricao_atualizacao), str(current_model.data_atualizacao), incidente_string, str(current_model.emails))
+                    #except Exception as e:
+                        #print(e)
                     current_model.save()
                 incidente_string = ""
     except Exception as e:
