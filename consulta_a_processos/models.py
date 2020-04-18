e3f11554a64fd4ba97a00c417faf24e368c27ba2
Cassio Fabius Cambraia Ribeiro <cassiofabius@gmail.com>
2020-04-17 23:07:14 -0300

    Update models.py

--- consulta_a_processos/models.py
+++ consulta_a_processos/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# Create your models here.
 from django.conf import settings
 from django.utils import timezone
 from django.urls import reverse
@@ -8,7 +7,6 @@ from django.urls import reverse
 # Ao adicionar uma nova classe, antes das migrações, um valor default deve ser acrescentado.
  
 class Processos(models.Model):
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     classe = models.TextField()
     numero = models.TextField()
     descricao = models.TextField()
@@ -17,8 +15,6 @@ class Processos(models.Model):
     descricao_atualizacao = models.TextField(default="desc_at", blank=True, null=True)
     emails = models.TextField(default="processosglobobsa@gmail.com", blank=True, null=True)
     url = models.TextField(default="url", blank = True)
-    #created_date = models.DateTimeField(default=timezone.now)
-    #published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
